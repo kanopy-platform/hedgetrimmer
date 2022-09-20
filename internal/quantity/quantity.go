58937a41e97fec74a31d152fc9b592519fd7e794
Yuzhou Liu <yuzhou.liu@mongodb.com>
2022-09-20 14:41:52 -0400

    Quantity management library

--- internal/quantity/quantity.go
+++ internal/quantity/quantity.go
@@ -6,35 +6,16 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 )
 
-// Add returns the result of x+y.
-// If true is returned, the operation overflowed and the result will be equal to x.
-func Add(x resource.Quantity, y resource.Quantity) (result resource.Quantity, overflow bool) {
-	overflow = false
-	result = x.DeepCopy()
+func Add(x resource.Quantity, y resource.Quantity) resource.Quantity {
+	result := x.DeepCopy()
 	result.Add(y)
-
-	if !y.IsZero() && result.Cmp(x) == 0 {
-		// TODO add to PR comment it seems overflow is "possible"
-		// https://github.com/kubernetes/apimachinery/blob/master/pkg/api/resource/quantity.go#L573
-		// however, like in the _test.go, I don't think it's possible for us to cause it
-		overflow = true
-	}
-
-	return
+	return result
 }
 
-// Add returns the result of x-y.
-// If true is returned, the operation overflowed and the result will be equal to x.
-func Sub(x resource.Quantity, y resource.Quantity) (result resource.Quantity, overflow bool) {
-	overflow = false
-	result = x.DeepCopy()
+func Sub(x resource.Quantity, y resource.Quantity) resource.Quantity {
+	result := x.DeepCopy()
 	result.Sub(y)
-
-	if !y.IsZero() && result.Cmp(x) == 0 {
-		overflow = true
-	}
-
-	return
+	return result
 }
 
 func Mul(x resource.Quantity, y resource.Quantity) resource.Quantity {
@@ -56,10 +37,5 @@ func MulFloat64(x resource.Quantity, y float64) (resource.Quantity, error) {
 
 func DivFloat64(x resource.Quantity, y float64) (resource.Quantity, error) {
 	yInverse := 1 / y
-	yQuantity, err := resource.ParseQuantity(fmt.Sprintf("%v", yInverse))
-	if err != nil {
-		return x, err
-	}
-
-	return Mul(x, yQuantity), nil
+	return MulFloat64(x, yInverse)
 }
--- internal/quantity/quantity_test.go
+++ internal/quantity/quantity_test.go
@@ -16,62 +16,45 @@ func TestAdd(t *testing.T) {
 	bigInt := big.NewInt(0).SetUint64(18446744073709551614)
 
 	tests := []struct {
-		inputA       resource.Quantity
-		inputB       resource.Quantity
-		want         resource.Quantity
-		wantOverflow bool
+		inputA resource.Quantity
+		inputB resource.Quantity
+		want   resource.Quantity
 	}{
 		{
-			inputA:       *resource.NewQuantity(15000, resource.BinarySI),
-			inputB:       *resource.NewQuantity(17000, resource.BinarySI),
-			want:         *resource.NewQuantity(32000, resource.BinarySI),
-			wantOverflow: false,
+			inputA: resource.MustParse("31.123456Gi"),
+			inputB: resource.MustParse("27.654321Gi"),
+			want:   resource.MustParse("58.777777Gi"),
 		},
 		{
 			// binary + decimal = binary
-			inputA:       *resource.NewQuantity(11, resource.BinarySI),
-			inputB:       *resource.NewQuantity(34, resource.DecimalSI),
-			want:         *resource.NewQuantity(45, resource.BinarySI),
-			wantOverflow: false,
+			inputA: *resource.NewQuantity(11, resource.BinarySI),
+			inputB: *resource.NewQuantity(34, resource.DecimalSI),
+			want:   *resource.NewQuantity(45, resource.BinarySI),
 		},
 		{
 			// 1200 + 0.01 = 1200.01
 			// inf.Scale is negated, -2 means *100, 2 means *0.01
-			inputA:       *resource.NewDecimalQuantity(*inf.NewDec(12, -2), resource.DecimalSI),
-			inputB:       *resource.NewDecimalQuantity(*inf.NewDec(1, 2), resource.DecimalSI),
-			want:         *resource.NewDecimalQuantity(*inf.NewDec(120001, 2), resource.DecimalSI),
-			wantOverflow: false,
+			inputA: *resource.NewDecimalQuantity(*inf.NewDec(12, -2), resource.DecimalSI),
+			inputB: *resource.NewDecimalQuantity(*inf.NewDec(1, 2), resource.DecimalSI),
+			want:   *resource.NewDecimalQuantity(*inf.NewDec(120001, 2), resource.DecimalSI),
 		},
 		{
 			// -10 + 0.00028 = -9.99972
-			inputA:       *resource.NewScaledQuantity(-1, 1),
-			inputB:       *resource.NewScaledQuantity(28, -5),
-			want:         *resource.NewScaledQuantity(-999972, -5),
-			wantOverflow: false,
-		},
-		{
-			// overflow testing, max int64 is 9.223372036854775807x10^18
-			// use 123400*10^18 + 987600*10^18
-			inputA:       *resource.NewScaledQuantity(123400, resource.Exa),
-			inputB:       *resource.NewScaledQuantity(987600, resource.Exa),
-			want:         *resource.NewScaledQuantity(1111000, resource.Exa),
-			wantOverflow: false,
+			inputA: *resource.NewScaledQuantity(-1, 1),
+			inputB: *resource.NewScaledQuantity(28, -5),
+			want:   *resource.NewScaledQuantity(-999972, -5),
 		},
 		{
 			// demonstrates for practical uses, cannot overflow
 			// [max(int64) x10^max(int32)] + [max(int64) x10^max(int32)]
-			inputA:       *resource.NewScaledQuantity(9223372036854775807, 2147483647),
-			inputB:       *resource.NewScaledQuantity(9223372036854775807, 2147483647),
-			want:         *resource.NewDecimalQuantity(*inf.NewDecBig(bigInt, -2147483647), resource.DecimalSI),
-			wantOverflow: false,
+			inputA: *resource.NewScaledQuantity(9223372036854775807, 2147483647),
+			inputB: *resource.NewScaledQuantity(9223372036854775807, 2147483647),
+			want:   *resource.NewDecimalQuantity(*inf.NewDecBig(bigInt, -2147483647), resource.DecimalSI),
 		},
 	}
 
 	for _, test := range tests {
-		result, overflow := Add(test.inputA, test.inputB)
-
-		assert.Equal(t, test.want, result)
-		assert.Equal(t, test.wantOverflow, overflow)
+		assert.Equal(t, test.want, Add(test.inputA, test.inputB))
 	}
 }
 
@@ -83,62 +66,45 @@ func TestSub(t *testing.T) {
 	bigInt = bigInt.Neg(bigInt)
 
 	tests := []struct {
-		inputA       resource.Quantity
-		inputB       resource.Quantity
-		want         resource.Quantity
-		wantOverflow bool
+		inputA resource.Quantity
+		inputB resource.Quantity
+		want   resource.Quantity
 	}{
 		{
-			inputA:       resource.MustParse("31Gi"),
-			inputB:       resource.MustParse("27.2Gi"),
-			want:         resource.MustParse("3.8Gi"),
-			wantOverflow: false,
+			inputA: resource.MustParse("31Gi"),
+			inputB: resource.MustParse("27.2Gi"),
+			want:   resource.MustParse("3.8Gi"),
 		},
 		{
 			// binary - decimal = binary
-			inputA:       resource.MustParse("55Ki"),
-			inputB:       resource.MustParse("5000"),
-			want:         *resource.NewQuantity(51320, resource.BinarySI),
-			wantOverflow: false,
+			inputA: resource.MustParse("55Ki"),
+			inputB: resource.MustParse("5000"),
+			want:   *resource.NewQuantity(51320, resource.BinarySI),
 		},
 		{
 			// 1200 - 0.01 = 1199.99
 			// inf.Scale is negated, -2 means *100, 2 mean *0.01
-			inputA:       *resource.NewDecimalQuantity(*inf.NewDec(12, -2), resource.DecimalSI),
-			inputB:       *resource.NewDecimalQuantity(*inf.NewDec(1, 2), resource.DecimalSI),
-			want:         *resource.NewDecimalQuantity(*inf.NewDec(119999, 2), resource.DecimalSI),
-			wantOverflow: false,
+			inputA: *resource.NewDecimalQuantity(*inf.NewDec(12, -2), resource.DecimalSI),
+			inputB: *resource.NewDecimalQuantity(*inf.NewDec(1, 2), resource.DecimalSI),
+			want:   *resource.NewDecimalQuantity(*inf.NewDec(119999, 2), resource.DecimalSI),
 		},
 		{
 			// 10 - 10.00028 = -0.00028
-			inputA:       *resource.NewScaledQuantity(1, 1),
-			inputB:       *resource.NewScaledQuantity(1000028, -5),
-			want:         *resource.NewScaledQuantity(-28, -5),
-			wantOverflow: false,
-		},
-		{
-			// shouldn't overflow, min int64 is -9.223372036854775808x10^18
-			// use 123400*10^18 - 987600*10^18
-			inputA:       *resource.NewScaledQuantity(123400, resource.Exa),
-			inputB:       *resource.NewScaledQuantity(987600, resource.Exa),
-			want:         *resource.NewScaledQuantity(-864200, resource.Exa),
-			wantOverflow: false,
+			inputA: *resource.NewScaledQuantity(1, 1),
+			inputB: *resource.NewScaledQuantity(1000028, -5),
+			want:   *resource.NewScaledQuantity(-28, -5),
 		},
 		{
 			// demonstrates for practical uses, cannot overflow
 			// [min(int64) x10^max(int32)] - [max(int64) x10^max(int32)]
-			inputA:       *resource.NewScaledQuantity(-9223372036854775808, 2147483647),
-			inputB:       *resource.NewScaledQuantity(9223372036854775807, 2147483647),
-			want:         *resource.NewDecimalQuantity(*inf.NewDecBig(bigInt, -2147483647), resource.DecimalSI),
-			wantOverflow: false,
+			inputA: *resource.NewScaledQuantity(-9223372036854775808, 2147483647),
+			inputB: *resource.NewScaledQuantity(9223372036854775807, 2147483647),
+			want:   *resource.NewDecimalQuantity(*inf.NewDecBig(bigInt, -2147483647), resource.DecimalSI),
 		},
 	}
 
 	for _, test := range tests {
-		result, overflow := Sub(test.inputA, test.inputB)
-
-		assert.Equal(t, test.want, result)
-		assert.Equal(t, test.wantOverflow, overflow)
+		assert.Equal(t, test.want, Sub(test.inputA, test.inputB))
 	}
 }
 
@@ -146,24 +112,53 @@ func TestMulFloat64(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		inputA    resource.Quantity
-		inputB    float64
-		want      resource.Quantity
-		wantError error
+		inputA resource.Quantity
+		inputB float64
+		want   resource.Quantity
+		// roundingPrecision is the decimal place to compare want vs result due to loss of precision
+		roundingPrecision int32
+		wantError         bool
 	}{
 		{
-			inputA:    resource.MustParse("31Gi"),
-			inputB:    2.525873,
-			want:      *resource.NewDecimalQuantity(*inf.NewDec(84076199948582912, 6), resource.BinarySI),
-			wantError: nil,
+			// negative multiplier
+			inputA:            resource.MustParse("501Mi"),
+			inputB:            -0.123456,
+			want:              *resource.NewDecimalQuantity(*inf.NewDec(-64855952326656, 6), resource.BinarySI),
+			roundingPrecision: -6, // 0.000001 precision
+		},
+		{
+			// positive multiplier
+			inputA:            resource.MustParse("31Gi"),
+			inputB:            2.525873,
+			want:              *resource.NewDecimalQuantity(*inf.NewDec(84076199948582912, 6), resource.BinarySI),
+			roundingPrecision: -6, // 0.000001 precision
+		},
+		{
+			// repeating decimal multiplier
+			inputA:            resource.MustParse("31Gi"),
+			inputB:            10000 / 7.0,
+			want:              *resource.NewDecimalQuantity(*inf.NewDec(475514236344, -2), resource.BinarySI),
+			roundingPrecision: 2, // 10^2 precision
+		},
+		{
+			// overflow testing
+			inputA:            resource.MustParse("5Ei"),
+			inputB:            95.05,
+			want:              *resource.NewDecimalQuantity(*inf.NewDec(5479259450644040254, -2), resource.BinarySI),
+			roundingPrecision: 2, // 10^2 precision
 		},
 	}
 
 	for _, test := range tests {
 		result, err := MulFloat64(test.inputA, test.inputB)
+		if test.wantError {
+			assert.Error(t, err)
+		} else {
+			result.RoundUp(resource.Scale(test.roundingPrecision))
+			test.want.RoundUp(resource.Scale(test.roundingPrecision))
 
-		assert.Equal(t, test.want, result)
-		assert.Equal(t, test.wantError, err)
+			assert.Equal(t, test.want, result)
+		}
 	}
 }
 
@@ -173,26 +168,42 @@ func TestDivFloat64(t *testing.T) {
 	tests := []struct {
 		inputA            resource.Quantity
 		inputB            float64
-		roundingPrecision int32
 		want              resource.Quantity
-		wantError         error
+		roundingPrecision int32
+		wantError         bool
 	}{
 		{
+			// negative divisor
+			inputA:            resource.MustParse("256Mi"),
+			inputB:            -1 / 6.0,
+			want:              *resource.NewDecimalQuantity(*inf.NewDec(-1610612736, 0), resource.BinarySI),
+			roundingPrecision: 0,
+		},
+		{
+			// positive divisor
 			inputA:            resource.MustParse("31Gi"),
 			inputB:            2.525873,
-			roundingPrecision: 3, // KiB precision
 			want:              *resource.NewDecimalQuantity(*inf.NewDec(13178016687, 0), resource.BinarySI),
-			wantError:         nil,
+			roundingPrecision: 3, // KiB precision
+		},
+		{
+			// repeating decimal divisor
+			inputA:            resource.MustParse("5128Ti"),
+			inputB:            10.0 / 6.0,
+			want:              *resource.NewDecimalQuantity(*inf.NewDec(33829773763411968, 1), resource.BinarySI),
+			roundingPrecision: -1, // 0.1 precision
 		},
 	}
 
 	for _, test := range tests {
 		result, err := DivFloat64(test.inputA, test.inputB)
+		if test.wantError {
+			assert.Error(t, err)
+		} else {
+			result.RoundUp(resource.Scale(test.roundingPrecision))
+			test.want.RoundUp(resource.Scale(test.roundingPrecision))
 
-		result.RoundUp(resource.Scale(test.roundingPrecision))
-		test.want.RoundUp(resource.Scale(test.roundingPrecision))
-
-		assert.Equal(t, test.want, result)
-		assert.Equal(t, test.wantError, err)
+			assert.Equal(t, test.want, result)
+		}
 	}
 }
