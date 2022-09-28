f2f061274126ed201ee4fda919899bd721e6b221
William Walter <53450727+whwalter@users.noreply.github.com>
2022-09-28 16:29:10 -0400

    Limit Range Config Abstraction (#6)
    * add limitranger interface and implementation
    
    * update interface signature for pointer return and error
    
    * refactor NewConfig to LimitRangeConfig

---
+++ internal/admission/limitrange.go
@@ -0,0 +1,11 @@
+package admission
+
+import (
+	"github.com/kanopy-platform/hedgetrimmer/internal/limitrange"
+	corev1Listers "k8s.io/client-go/listers/core/v1"
+)
+
+type LimitRanger interface {
+	corev1Listers.LimitRangeLister
+	LimitRangeConfig(namespace string) (*limitrange.Config, error)
+}
