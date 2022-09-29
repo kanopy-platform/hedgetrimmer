277368d86ee2b86935b740af62a57ed3c97b2fc4
wwalter <william.walter@mongodb.com>
2022-09-29 08:56:07 -0400

    sts handler, resolved branch conflict with limitrange, add swp to gitignore

---
+++ internal/admission/sts.go
@@ -0,0 +1,75 @@
+package admission
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+type STSHandler struct {
+	decoder *admission.Decoder
+	ptm     PodTemplateSpecMutator
+	lr      LimitRanger
+}
+
+func NewSTSHandler(ptm PodTemplateSpecMutator, lr LimitRanger) *STSHandler {
+	return &STSHandler{ptm: ptm, lr: lr}
+}
+
+func (sts *STSHandler) Kind() string { return "StatefulSet" }
+
+func (sts *STSHandler) InjectDecoder(d *admission.Decoder) error {
+	sts.decoder = d
+	return nil
+}
+
+func (sts *STSHandler) Handle(ctx context.Context, req admission.Request) admission.Response {
+	log := log.FromContext(ctx)
+
+	in := &appsv1.StatefulSet{}
+	err := sts.decoder.Decode(req, in)
+	if err != nil {
+		log.Error(err, fmt.Sprintf("Failed to decode statefulset requests: %s", req.Name))
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	lrConfig, err := sts.lr.LimitRangeConfig(in.Namespace)
+	if err != nil {
+		reason := fmt.Sprintf("Failed to list LimitRanges in namespace: %s", in.Namespace)
+		log.Error(err, reason)
+		//If we cannot get LimitRanges due to an api error fail.
+		return admission.Denied(reason)
+	}
+
+	if lrConfig == nil {
+		// If there are no Limit Ranges in the target namespace, workloads go un-mutated
+		return admission.Allowed(fmt.Sprintf("No limit range in namespace: %s", in.Namespace))
+	}
+
+	var out appsv1.StatefulSet
+	in.DeepCopyInto(&out)
+
+	pts, err := sts.ptm.Mutate(out.Spec.Template, *lrConfig)
+	if err != nil {
+		reason := fmt.Sprintf("Failed to mutate statefulset %s/%s: %s", in.Namespace, in.Name, err)
+		log.Error(err, reason)
+		return admission.Denied(reason)
+	}
+
+	out.Spec.Template = pts
+
+	jout, err := json.Marshal(out)
+	if err != nil {
+		reason := fmt.Sprintf("Failed to marhsal statefulset %s/%s: %s", in.Namespace, in.Name, err)
+		log.Error(err, reason)
+		return admission.Denied(reason)
+	}
+
+	return admission.PatchResponseFromRaw(req.Object.Raw, jout)
+
+}
---
+++ internal/admission/sts_test.go
@@ -0,0 +1,137 @@
+package admission
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/kanopy-platform/hedgetrimmer/internal/limitrange"
+	"github.com/stretchr/testify/assert"
+	admissionv1 "k8s.io/api/admission/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	corev1Listers "k8s.io/client-go/listers/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+type MockLimitRanger struct {
+	lrc *limitrange.Config
+	err error
+}
+
+func (mlr *MockLimitRanger) SetConfig(lrc *limitrange.Config) {
+	mlr.lrc = lrc
+}
+
+func (mlr *MockLimitRanger) SetErr(err error) {
+	mlr.err = err
+}
+
+func (mlr *MockLimitRanger) List(selector labels.Selector) (ret []*corev1.LimitRange, err error) {
+	return
+}
+func (mlr *MockLimitRanger) LimitRanges(namespace string) corev1Listers.LimitRangeNamespaceLister {
+	return nil
+}
+
+func (mlr *MockLimitRanger) LimitRangeConfig(namespace string) (*limitrange.Config, error) {
+	return mlr.lrc, mlr.err
+}
+
+type MockMutator struct {
+	spec corev1.PodTemplateSpec
+	err  error
+}
+
+func (mm *MockMutator) SetSpec(spec corev1.PodTemplateSpec) {
+	mm.spec = spec
+}
+
+func (mm *MockMutator) SetErr(err error) {
+	mm.err = err
+}
+
+func (mm *MockMutator) Mutate(inputs corev1.PodTemplateSpec, config limitrange.Config) (corev1.PodTemplateSpec, error) {
+	return mm.spec, mm.err
+}
+
+func TestSTSHandler(t *testing.T) {
+	t.Parallel()
+	mm := MockMutator{}
+	mlr := MockLimitRanger{}
+
+	scheme := runtime.NewScheme()
+	decoder, err := admission.NewDecoder(scheme)
+	assert.NoError(t, err)
+
+	handler := NewSTSHandler(&mm, &mlr)
+
+	assert.NoError(t, handler.InjectDecoder(decoder))
+
+	sts := appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-sts",
+			Namespace: "test-ns",
+		},
+		Spec: appsv1.StatefulSetSpec{},
+	}
+
+	stsBytes, err := json.Marshal(sts)
+	assert.NoError(t, err)
+
+	ar := admissionv1.AdmissionRequest{
+		Object: runtime.RawExtension{
+			Raw: stsBytes,
+		},
+	}
+
+	tests := []struct {
+		config  *limitrange.Config
+		lrerr   error
+		pts     corev1.PodTemplateSpec
+		merr    error
+		reject  bool
+		msg     string
+		mutated bool
+	}{
+		{
+			lrerr:  fmt.Errorf("Fail"),
+			reject: true,
+			msg:    "Deny for lr error",
+		},
+		{
+			msg: "Allow for a namespace with no limitranges",
+		},
+		{
+			reject: true,
+			config: &limitrange.Config{},
+			merr:   fmt.Errorf("Fail"),
+			msg:    "Reject for failed mutation",
+		},
+		{
+			config: &limitrange.Config{},
+			pts: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "mutated",
+				},
+			},
+			msg:     "Allow for a namespace with no limitranges",
+			mutated: true,
+		},
+	}
+
+	for _, test := range tests {
+		mlr.SetConfig(test.config)
+		mlr.SetErr(test.lrerr)
+		mm.SetSpec(test.pts)
+		mm.SetErr(test.merr)
+
+		resp := handler.Handle(context.TODO(), admission.Request{AdmissionRequest: ar})
+		assert.Equal(t, test.reject, !resp.Allowed, test.msg)
+		assert.Equal(t, test.mutated, len(resp.Patches) > 0)
+	}
+}
