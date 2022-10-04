e63b9513e5e9e646ed5d0dc05426875441dc68de
Yuzhou Liu <89029096+yuzhouliu9@users.noreply.github.com>
2022-10-04 14:41:56 -0400

    ReplicationController handler (#19)

---
+++ pkg/admission/handlers/replicationcontroller.go
@@ -0,0 +1,56 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/kanopy-platform/hedgetrimmer/pkg/admission"
+	"github.com/kanopy-platform/hedgetrimmer/pkg/limitrange"
+	kadmission "sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+type ReplicationControllerHandler struct {
+	DefaultDecoderInjector
+	AllVersionSupporter
+	ptm admission.PodTemplateSpecMutator
+}
+
+func NewReplicationControllerHandler(ptm admission.PodTemplateSpecMutator) *ReplicationControllerHandler {
+	return &ReplicationControllerHandler{ptm: ptm}
+}
+
+func (j *ReplicationControllerHandler) Kind() string {
+	return "ReplicationController"
+}
+
+func (j *ReplicationControllerHandler) Handle(ctx context.Context, req kadmission.Request) kadmission.Response {
+	log := log.FromContext(ctx)
+
+	lrConfig, err := limitrange.MemoryConfigFromContext(ctx)
+	if err != nil {
+		reason := fmt.Sprintf("invalid LimitRange config for namespace: %s", req.Namespace)
+		log.Error(err, reason)
+		return kadmission.Denied(reason)
+	}
+
+	out := &corev1.ReplicationController{}
+	if err := j.decoder.Decode(req, out); err != nil {
+		log.Error(err, "failed to decode ReplicationController request: %s", req.Name)
+		return kadmission.Errored(http.StatusBadRequest, err)
+	}
+
+	pts, err := j.ptm.Mutate(*out.Spec.Template, lrConfig)
+	if err != nil {
+		reason := fmt.Sprintf("failed to mutate ReplicationController %s/%s: %s", out.Namespace, out.Name, err)
+		log.Error(err, reason)
+		return kadmission.Denied(reason)
+	}
+
+	out.Spec.Template = &pts
+
+	return PatchResponse(req.Object.Raw, out)
+}
